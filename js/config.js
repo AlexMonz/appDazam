471489e5fc9cee7073fce9a69ff3c8972c228147
Alex Monzón <alexmonzonp@gmail.com>
2017-05-27 18:13:37 +0200

    Enlaces paginas.

--- js/config.js
+++ js/config.js
@@ -10,8 +10,17 @@ app.config( function ($routeProvider) {
 		.when('/Contacto', {
 			templateUrl: 'paginas/contacto.html'
 		})
-		.when('/', {
-			templateUrl: 'paginas/home.html'
+		.when('/Calderas', {
+			templateUrl: 'paginas/calderas.html'
+		})
+		.when('/Calefaccion', {
+			templateUrl: 'paginas/calefaccion.html'
+		})
+		.when('/Instalaciones', {
+			templateUrl: 'paginas/instalaciones.html'
+		})
+		.when('/Login', {
+			templateUrl: 'paginas/login.html'
 		})
 		.otherwise({ redirectTo: '/' })
 });
