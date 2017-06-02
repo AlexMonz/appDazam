db3259261f3ce1cde93b60f9058063656445f164
Alex Monzón <alexmonzonp@gmail.com>
2017-06-02 22:22:44 +0200

    get a base de datos ok

--- js/tiendaCtrl.js
+++ js/tiendaCtrl.js
@@ -1,46 +1,19 @@
-var data  = [
-  {
-    "_id": "592ee95eeff218c875bc2b63",
-    "index": 0,
-    "name": "Wagner Berger",
-    "picture": "http://placehold.it/32x32",
-    "age": 34
-  },
-  {
-    "_id": "592ee95e3d70ab19947c568d",
-    "index": 1,
-    "name": "Oneal Barber",
-    "picture": "http://placehold.it/32x32",
-    "age": 26
-  },
-  {
-    "_id": "592ee95e200e64342dd67a68",
-    "index": 2,
-    "name": "Pearson Horne",
-    "picture": "http://placehold.it/32x32",
-    "age": 26
-  },
-  {
-    "_id": "592ee95ec5c6fab89e7a1560",
-    "index": 3,
-    "name": "Rochelle Garrison",
-    "picture": "http://placehold.it/32x32",
-    "age": 40
-  },
-  {
-    "_id": "592ee95e2ac64db1c7defa0b",
-    "index": 4,
-    "name": "Wolf Joyner",
-    "picture": "http://placehold.it/32x32",
-    "age": 36
-  }
-]
 
-
-
-app.controller('tiendaCtrl', ['$scope', function($scope){
+app.controller('tiendaCtrl', ['$scope', '$http', function($scope,$http){
   
-   $scope.productos = data;
+ $scope.productos = {};
+
+$http.get('php/servicios/productos.listado.php')
+
+.then(function(response) {
+
+	var data = response.data;
+    
+	$scope.productos = data;
+
+})
  
- 
-}])
+}]);
+
+
+
